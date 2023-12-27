dc57cb46b8243327104a4c0eb3802e3b7c7bada7
Juliano Sousa <julianosousasilveira@gmail.com>
2023-12-26 23:35:03 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles.css'; // Importa o arquivo de estilos CSS
 
-function App() {
+
+const CatImage = () => {
+  const [catImage, setCatImage] = useState('');
+
+  useEffect(() => {
+    const fetchCatImage = async () => {
+      try {
+        const response = await axios.get('https://api.thecatapi.com/v1/images/search');
+        const imageUrl = response.data[0]?.url || '';
+        setCatImage(imageUrl);
+      } catch (error) {
+        console.error('Erro ao buscar imagem do gato:', error);
+      }
+    };
+
+    fetchCatImage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main-container">
+        <div className="content-container">
+          <p className="birthday-text">Happy</p>
+          <p className="birthday-text">Birthday</p>
+          {catImage && (
+              <img
+                  src={catImage}
+                  alt="Cat"
+                  className="cat-image"
+              />
+          )}
+          <div className="div-texto">
+          <p className= "texto">
+            BOM, GATINHA, ESSA É A PRIMEIRA VERSÃO, MAS EU QUERIA TE DESEJAR UM FELIZ ANIVERSÁRIO, MUITOS ANOS DE VIDA E TUDO DE BOM!
+          </p>
+          </div>
+        </div>
+      </div>
   );
-}
+};
 
-export default App;
+export default CatImage;
