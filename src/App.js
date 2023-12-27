ba03d3c14167d4c0acb14e799629169c030e8b86
Juliano Sousa <julianosousasilveira@gmail.com>
2023-12-27 00:31:03 -0300

    v1.1

--- src/App.js
+++ src/App.js
@@ -6,32 +6,35 @@ import './styles.css'; // Importa o arquivo de estilos CSS
 const CatImage = () => {
   const [catImage, setCatImage] = useState('');
 
-  useEffect(() => {
-    const fetchCatImage = async () => {
-      try {
-        const response = await axios.get('https://api.thecatapi.com/v1/images/search');
-        const imageUrl = response.data[0]?.url || '';
-        setCatImage(imageUrl);
-      } catch (error) {
-        console.error('Erro ao buscar imagem do gato:', error);
-      }
-    };
+  const fetchCatImage = async () => {
+    setCatImage('')
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search');
+      const imageUrl = response.data[0]?.url ?? '';
+      setCatImage(imageUrl);
+    } catch (error) {
+      console.error('Erro ao buscar imagem do gato:', error);
+    }
+  };
 
+  useEffect(() => {
     fetchCatImage();
   }, []);
-
+  console.log(typeof catImage === 'undefined')
   return (
       <div className="main-container">
         <div className="content-container">
           <p className="birthday-text">Happy</p>
           <p className="birthday-text">Birthday</p>
-          {catImage && (
-              <img
-                  src={catImage}
+          <div className='div-img'>
+          {catImage === '' ? <div className={'loader center'}></div>
+              :<img
+                  src={(catImage)}
                   alt="Cat"
-                  className="cat-image"
-              />
-          )}
+                  onClick={fetchCatImage}
+                  className="cat-image"/>
+              }
+          </div>
           <div className="div-texto">
           <p className= "texto">
             BOM, GATINHA, ESSA É A PRIMEIRA VERSÃO, MAS EU QUERIA TE DESEJAR UM FELIZ ANIVERSÁRIO, MUITOS ANOS DE VIDA E TUDO DE BOM!
